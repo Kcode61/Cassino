0c7814a3e4a73a9cf2efd08d1823a73ccbc57a2a
Kauan Moura <seuemail@gmail.com>
2026-07-13 20:32:19 -0300

    updated pom.xml and removed google login button in the frontend

--- Frontend/app/register/page.tsx
+++ Frontend/app/register/page.tsx
@@ -60,19 +60,6 @@ export default function register() {
               <p className="text-[#717C8E] mb-6 text-sm">
                 Cadastre-se e comece a jogar
               </p>
-              <button className="py-3 px-4 gap-4 bg-[#11131A] cursor-pointer hover:bg-[#1A1D25] text-sm text-white font-inter w-full flex items-center justify-center rounded-xl font-semibold border border-[#2A2D35]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="bi bi-google"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z" />
-                </svg>
-                Continuar com Google
-              </button>
 
               <div className="flex gap-4 items-center py-5">
                 <div className="w-full h-px  bg-[#2A2E39]  "></div>
